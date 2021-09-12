6d89b930863166e6ee3bbb7fb8540bff6d932a4a
coolgamer362 <mrdope3623@gmail.com>
2021-09-12 22:45:18 +0530

    backround changed to black

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
-import { useState } from 'react';
+//import { useState } from 'react';
 import './App.css';
 import Header from './Components/Header';
 import Body from './Pages/Body';
-import Login from './Pages/Login';
+//import Login from './Pages/Login';
 
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false)
+  //const [loggedIn, setLoggedIn] = useState(false)
   return (
     <div className="App">
-      {
-        loggedIn ? <div className="app">
+      
+         <div className="app">
           <Header/>
           <Body/>
-        </div> :
-        <Login setLoggedIn={setLoggedIn} />
-      }
+        </div> 
+        
+      
     </div>
   );
 }
