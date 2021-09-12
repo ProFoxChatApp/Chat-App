90c55e128585cf884500dc22d4fe9fc4992eb2b0
Xhackers7 <nishaajithkj@gmail.com>
2021-09-12 15:20:45 +0530

    Finished the base design

--- client/src/Components/ChatBox.js
+++ client/src/Components/ChatBox.js
@@ -8,6 +8,7 @@ function ChatBox() {
             </div>
             <div className="msgArea">
                 <input type="text" placeholder="Message Someone" className="textBox"/>
+                <button className="sendBtn">Send</button>
             </div>
         </div>
     )
--- client/src/Components/ChatList.js
+++ client/src/Components/ChatList.js
@@ -3,76 +3,92 @@ import React from 'react'
 function ChatList() {
     return (
         <div className="chatList">
-            
-           <div className="name">
-               Someone
-           </div>
 
-           <div className="nme">
-               Someone else
-           </div>
+            <div className="active">
+                <img src="https://wallpapercave.com/wp/wp6686191.jpg" alt="" className="pfp" />
+                Someone
+            </div>
 
-           <div className="name">
-               Someone
-           </div>
+            <div className="name">
+                <img src="https://wallpapercave.com/wp/wp6686191.jpg" alt="" className="pfp" />
+                Someone else
+            </div>
 
-           <div className="nme">
-               Someone else
-           </div>
+            <div className="name">
+                <img src="https://wallpapercave.com/wp/wp6686191.jpg" alt="" className="pfp" />
+                Someone
+            </div>
 
-           <div className="name">
-               Someone
-           </div>
+            <div className="name">
+                <img src="https://wallpapercave.com/wp/wp6686191.jpg" alt="" className="pfp" />
+                Someone else
+            </div>
 
-           <div className="nme">
-               Someone else
-           </div>
+            <div className="name">
+                <img src="https://wallpapercave.com/wp/wp6686191.jpg" alt="" className="pfp" />
+                Someone
+            </div>
 
-           <div className="name">
-               Someone
-           </div>
+            <div className="name">
+                <img src="https://wallpapercave.com/wp/wp6686191.jpg" alt="" className="pfp" />
+                Someone else
+            </div>
 
-           <div className="nme">
-               Someone else
-           </div>
+            <div className="name">
+                <img src="https://wallpapercave.com/wp/wp6686191.jpg" alt="" className="pfp" />
+                Someone
+            </div>
+
+            <div className="name">
+                <img src="https://wallpapercave.com/wp/wp6686191.jpg" alt="" className="pfp" />
+                Someone else
+            </div>
 
 
 
-           <div className="name">
-               Someone
-           </div>
+            <div className="name">
+                <img src="https://wallpapercave.com/wp/wp6686191.jpg" alt="" className="pfp" />
+                Someone
+            </div>
 
-           <div className="nme">
-               Someone else
-           </div>
+            <div className="name">
+                <img src="https://wallpapercave.com/wp/wp6686191.jpg" alt="" className="pfp" />
+                Someone else
+            </div>
 
-           <div className="name">
-               Someone
-           </div>
+            <div className="name">
+                <img src="https://wallpapercave.com/wp/wp6686191.jpg" alt="" className="pfp" />
+                Someone
+            </div>
 
-           <div className="nme">
-               Someone else
-           </div>
+            <div className="name">
+                <img src="https://wallpapercave.com/wp/wp6686191.jpg" alt="" className="pfp" />
+                Someone else
+            </div>
 
-           <div className="name">
-               Someone
-           </div>
+            <div className="name">
+                <img src="https://wallpapercave.com/wp/wp6686191.jpg" alt="" className="pfp" />
+                Someone
+            </div>
 
-           <div className="nme">
-               Someone else
-           </div>
+            <div className="name">
+                <img src="https://wallpapercave.com/wp/wp6686191.jpg" alt="" className="pfp" />
+                Someone else
+            </div>
 
-           <div className="name">
-               Someone
-           </div>
+            <div className="name">
+                <img src="https://wallpapercave.com/wp/wp6686191.jpg" alt="" className="pfp" />
+                Someone
+            </div>
 
-           <div className="nme">
-               Someone else
-           </div>
-                
-            
-            
-            
+            <div className="name">
+                <img src="https://wallpapercave.com/wp/wp6686191.jpg" alt="" className="pfp" />
+                Someone else
+            </div>
+
+
+
+
         </div>
     )
 }
